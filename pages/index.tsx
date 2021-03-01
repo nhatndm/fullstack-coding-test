b23cf696470c72902c3fece23337f415eefea18c
Nhat Nguyen <nhatndm1193@gmail.com>
2021-03-01 14:54:34 +0800

    added Chark UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,27 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import DynamicText from "../components/DynamicText";
 
+import { Flex, Center, Input, Box } from "@chakra-ui/react";
+
 const Home = () => {
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
   };
 
   return (
-    <div className={styles.container}>
+    <Flex className={styles.container}>
       <Head>
         <title>Coding Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <DynamicText />
-        <input onChange={onChange} />
-      </main>
-    </div>
+      <Box width="20rem" className={styles.main}>
+        <Center flex={1} flexDirection="column">
+          <DynamicText />
+          <Input marginTop={15} variant="outline" onChange={onChange} />
+        </Center>
+      </Box>
+    </Flex>
   );
 };
 
